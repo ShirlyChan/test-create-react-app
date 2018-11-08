6f299fa7f7acec8eebefb936d4fdf0fdcd318e31
Shirly Chan <shirlyChan@gmail.com>
2018-11-08 18:30:37 +0800

    require antd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { hot } from 'react-hot-loader';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 import './App.css';
+import routes from './routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const APP = () => (
+  <LocaleProvider locale={zhCN}>
+    {routes}
+  </LocaleProvider>
+);
 
-export default App;
+export default hot(module)(APP);
+
